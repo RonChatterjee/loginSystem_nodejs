a5bcf046dc5947f5cf59d620b8ce5479b7033336
Arijit Chatterjee <ron13chatterjee@gmail.com>
2016-03-22 19:27:23 +0530

    Added facebook login feature using passport-facebook

--- app/routes.js
+++ app/routes.js
@@ -25,6 +25,11 @@ module.exports = function  (app,passport) {
 		failureRedirect : "/login",
 		failureFlash : true
 	}));
+	app.get("/auth/facebook", passport.authenticate('facebook',{ scope : ['email']}));
+	app.get("/auth/facebook/callback", passport.authenticate('facebook',{
+		successRedirect : "/profile",
+		failureRedirect : "/"
+	}));
 };
 function isLoggedIn (req,res,next) {
 	if(req.isAuthenticated())
---
+++ config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID' : '1692361627673367',
+		'clientSecret' : '120d1812cb696823c4d536f81661315a',
+		'callbackURL' : 'http://localhost:8080/auth/facebook/callback'
+	}
+};
